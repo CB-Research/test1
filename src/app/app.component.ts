19bcdd9cc59b10896feb0497772bce7bba9c1cd3
Victor Espinosa Mateu <victor.espinosa.mateu@everis.com>
2020-01-07 15:52:15 +0100

    testing call to docker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,23 @@ import * as L from 'leaflet';
 export class AppComponent implements AfterViewInit {
   title = 'test1';
 
+  constructor(private http: HttpClient) {
+
+  }
+
+  getData() {
+
+    // let heads = new HttpHeaders();
+    // heads = heads.set('Access-Control-Allow-Origin', '*');
+
+    this.http.get('/api/v2/entities/urn:ngsi-ld:Sensor:001').subscribe(res => {
+      console.log(res);
+    });
+  }
+
   ngAfterViewInit(): void {
     console.log('holi');
-    let map = L.map('map', {
+    const map = L.map('map', {
       center: [39.8282, -98.5795],
       zoom: 3
     });
@@ -23,6 +38,15 @@ export class AppComponent implements AfterViewInit {
 
     tiles.addTo(map);
 
+    map.setView([37.9922399, -1.1306544], 13);
+
+    const marker1 = L.marker([38.019988, -1.097616]).addTo(map);
+
+    const marker2 = L.marker([37.9922399, -1.1306544]).addTo(map);
+
+    marker1.bindPopup("Cerca Ca'ngel");
+
+    this.getData();
   }
 
 }
